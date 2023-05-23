91b4cf6ae535497c7f3e59840a7bb4454e00c553
Yeri Kim <laplacianmatrix@gmail.com>
2023-05-23 15:24:53 +0900

    chore: update `$ALLOWED_HOSTS`

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 PATH=/opt/x-tools8/aarch64-unknown-linux-gnu/bin:$PATH
-distccd --log-level info --log-stderr --allow-private --allow 192.168.0.0/24 --daemon --verbose --no-detach --port 3632 --jobs $DISTCCD_JOBS
+distccd --log-level info --log-stderr --allow-private --allow $ALLOWED_HOSTS --daemon --verbose --no-detach --port 3632 --jobs $DISTCCD_JOBS
